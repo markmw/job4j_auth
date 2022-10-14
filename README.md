f93f9e8947dd37fa76fb2b95ee489904bfad7534
markmw <dant1kkz@gmail.com>
2022-10-14 17:14:18 +0600

    1. RESTFull. Описание архитектуры [#6884]

---
+++ README.md
@@ -0,0 +1,9 @@
+#job4j_auth
+
+REST Сервис - JOB4J_AUTH
+
+Используется:
+1. Spring Boot
+2. Maven
+3. Spring Initialzr
+4. JDK 17
---
+++ src/main/java/ru/job4j/controller/PersonController.java
@@ -0,0 +1,56 @@
+package ru.job4j.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.domain.Person;
+import ru.job4j.repository.PersonRepository;
+import ru.job4j.service.PersonService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping("/")
+    public List<Person> findAll() {
+        return this.personService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> findById(@PathVariable int id) {
+        var person = this.personService.findById(id);
+        return new ResponseEntity<Person>(
+                person.orElse(new Person()),
+                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
+        );
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Person> create(@RequestBody Person person) {
+        return new ResponseEntity<Person>(
+                this.personService.save(person),
+                HttpStatus.CREATED
+        );
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Void> update(@RequestBody Person person) {
+        this.personService.save(person);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        Person person = new Person();
+        person.setId(id);
+        this.personService.delete(person);
+        return ResponseEntity.ok().build();
+    }
+}
